e2a34e7be8936ab14968c6ae01a417a52fff9019
sologuboved <sologuboved@gmail.com>
2026-02-22 15:10:18 +0300

    WIP

--- app/src/main/java/org/sologuboved/frflashcards/ui/theme/Color.kt
+++ app/src/main/java/org/sologuboved/frflashcards/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package org.sologuboved.frflashcards.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// val Purple40 = Color(0xFF6650a4)
+// val PurpleGrey40 = Color(0xFF625b71)
+// val Pink40 = Color(0xFF7D5260)
 
 // Matrix terminal colors - fully opaque (0xFF prefix)
 val DarkBackgroundColor = Color(0xFF26252b)      // Pure black
